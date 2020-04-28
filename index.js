a825033ba66e3bf5c745ae9a2def614133f1fbd9
Akhila Suraj <akhila.abesinghe@gmail.com>
2020-04-28 17:50:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+//import routes
+const authRoute = require("./routes/auth");
+const postRoute = require("./routes/posts");
+
+dotenv.config();
+
+//connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => console.log("connected to DB")
+);
+
+//Middleware
+app.use(express.json());
+
+//Route middleware
+app.use("/api/user", authRoute);
+app.use("/api/posts", postRoute);
+
+app.listen(3000, () => console.log("Server is up and running"));
